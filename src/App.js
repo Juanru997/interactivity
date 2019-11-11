0e53ed540daaf5d28fda18c431f101c40edd5a60
Juanru <yz5026@nyu.edu>
2019-11-11 18:23:22 +0800

    Button solved

--- src/App.js
+++ src/App.js
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    return (
+      <div className="App">
+        <header className="Body">
+
+          <Diyi />
+          <Dier />
+          <Disan />
+        </header>
+      </div>
+    );
+
+}
+
+
+function Diyi() {
+  // Declare a new state variable, which we'll call "count"
+  
+  const [first, setFirst] = useState(0);
+
+  function handleClick() {
+    setFirst(first + 1);
+  } 
+
+  if(first > 100) {
+    return 
+  } else {
+    return (
+    <div>
+     
+      <button className="hotbutton" onClick={handleClick}>
+        first
+      </button>
+
+      <p>You clicked {first} times</p>
     </div>
   );
 }
+}
+
+function Dier() {
+  // Declare a new state variable, which we'll call "count"
+  
+  const [second, setSecond] = useState(0);
+
+  function handleClick() {
+    setSecond(second + 1);
+  } 
+
+  if(second > 100) {
+    return 
+  } else {
+    return (
+    <div>
+     
+      <button className="hotbutton" onClick={handleClick}>
+        second
+      </button>
+
+      <p>You clicked {second} times</p>
+    </div>
+  );
+}
+}
+
+function Disan() {
+  // Declare a new state variable, which we'll call "count"
+  
+  const [san, setSan] = useState(0);
+
+  function handleClick() {
+    setSan(san + 1);
+  } 
+
+  if(san > 100) {
+    return 
+  } else {
+    return (
+    <div>
+     
+      <button className="hotbutton" onClick={handleClick}>
+        san
+      </button>
+
+      <p>You clicked {san} times</p>
+    </div>
+  );
+}
+}
+
+
+// function firstButton() {
+//   // Declare a new state variable, which we'll call "count"
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount(count + 1);
+//   } 
+
+//   if(count > 100) {
+//     return 
+//   } else {
+//     return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button className="hotbutton" onClick={handleClick}>
+//         Click me
+//       </button>
+//     </div>
+//   );
+// }
+// }
 
 export default App;
